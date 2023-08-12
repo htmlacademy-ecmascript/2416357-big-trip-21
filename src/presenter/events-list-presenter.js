587f88cdc2d85544933e18bc39b763859b0c2643
GussevPM <GussevPM@GMail.Com>
2023-08-12 16:25:14 +0400

    add views and presenter

---
+++ src/presenter/events-list-presenter.js
@@ -0,0 +1,21 @@
+import EventsListView from '../view/events-list-view';
+import EventEditView from '../view/event-edit-view';
+import EventView from '../view/event-in-list-view';
+import { render } from '../render';
+
+export default class EventsListPresenter {
+    eventsListComponent = new EventsListView();
+
+    constructor(eventsListSection) {
+        this.eventsListSection = eventsListSection;
+    }
+
+    init() {
+        render(this.eventsListComponent, this.eventsListSection);
+        render(new EventEditView(), this.eventsListComponent.getElement());
+
+        for (let i = 0; i < 3; i++) {
+            render(new EventView(), this.eventsListComponent.getElement());
+        }
+    }
+}
